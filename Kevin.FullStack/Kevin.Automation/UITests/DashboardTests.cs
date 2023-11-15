8f5a633dac7a6899225ba53482ca5515f2ca5667
Kevin Sejin <kevinricar24@hotmail.com>
2023-11-15 11:35:55 -0500

    upload automation

---
+++ Kevin.FullStack/Kevin.Automation/UITests/DashboardTests.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Kevin.Automation.UITests
+{
+    public class DashboardTests
+    {
+        private WebDriver driver;
+
+        public DashboardTests()
+        {
+            var options = new ChromeOptions();
+            options.BrowserVersion = "113";
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://kevinfullstackangular.azurewebsites.net/");
+        }
+
+        [Fact]
+        public void CompareTitle_ShouldBeEquals()
+        {
+            //Arrange
+            string titleExpected = "KevinAngular";
+            string titleActual = string.Empty;
+
+            //Act
+            titleActual = driver.Title;
+
+            //Assert
+            Assert.Equal(titleExpected, titleActual);
+
+            driver.Quit();
+        }
+
+        [Fact]
+        public void CreateEmployee_ShouldBeOk()
+        {
+            //Arrange
+            string titleExpected = "KevinAngular";
+            string titleActual = string.Empty;
+
+            //Act
+            var buttonCreate = driver.FindElement(By.XPath("/html[1]/body[1]/app-root[1]/div[1]/button[1]/span[2]"));
+            buttonCreate.Click();
+
+            var buttonCreate = driver.FindElement(By.XPath(""));
+            var buttonCreate = driver.FindElement(By.XPath(""));
+            var buttonCreate = driver.FindElement(By.XPath("/html[1]/body[1]/app-root[1]/div[1]/button[1]/span[2]"));
+
+            //Assert
+            Assert.Equal(titleExpected, titleActual);
+
+            driver.Quit();
+        }
+
+
+    }
+}
